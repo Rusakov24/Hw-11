fe19e7200aa991de3419f152a9f523949861de0f
Rusakov24 <lovewillkillme@vk.com>
2024-07-22 17:56:33 +0300

    Начало работы

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nЗадание 1");
+
+        System.out.println("\nЗадание 2");
+
+        System.out.println("\nЗадание 3");
+
+    }
+}
